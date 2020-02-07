2a733b6e16eb8a90eb4bef5f90206b2475900fee
Igor Music <igormusich@gmail.com>
2020-02-07 06:52:48 -0500

    initial commit

---
+++ src/main/java/com/tvmsoftware/api/Greeting.java
@@ -0,0 +1,12 @@
+package com.tvmsoftware.api;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Greeting {
+    private final long id;
+    private final String content;
+}
